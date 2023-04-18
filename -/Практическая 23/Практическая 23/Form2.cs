b39f70f0aaf0540d09b82fefe5357eb8f92f8588
ATOM <92225382+ATOM-WoT@users.noreply.github.com>
2023-04-18 16:41:55 +0700

    Pr23-24

---
+++ -/Практическая 23/Практическая 23/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическая_23
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Form1 Main = this.Owner as Form1;
+        }
+
+        private void lblDevelopers_2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
